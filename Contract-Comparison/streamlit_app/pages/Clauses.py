d084bf63b9e2aee7f996d0cab17e1dd3b6f0a990
Prem Kiran Laknaboina <premlaknaboina@gmail.com>
2024-07-13 16:08:59 +0000

    Final prototype

--- Contract-Comparison/streamlit_app/pages/Clauses.py
+++ Contract-Comparison/streamlit_app/pages/Clauses.py
@@ -1,31 +1,33 @@
 import streamlit as st
 from utils import extract_clauses
-from utils import llm
+from rag import query_output
 
 
 def main():
-    st.title("Contract comparator App")
+    st.title("Clauses App")
 
-    prompt = st.chat_input("If you have any doubts about clauses, let us know!")
-    if prompt:
-        st.write(f"User has sent the following prompt: {prompt}")
-        response = llm.complete(prompt)
-        st.write(response.text)
-    
-
-
-    if st.session_state['contract_text'] != '':
-        with st.spinner('Extracting Contract...'):
-            clauses = extract_clauses(st.session_state['contract_text'])
-        st.success("Done!")
-        st.toast('Contract extracted!')
+    if 'contract_text' in st.session_state:
+        if 'clauses' not in st.session_state:
+            with st.spinner('Extracting Clauses...'):
+                st.session_state['clauses'] = extract_clauses(st.session_state['contract_text'])
+            st.success("Done!")
+            st.toast('Clauses extracted!')
 
 
-        st.markdown(clauses.text)
+        st.markdown(st.session_state['clauses'].text)
     else:
         st.markdown("# Go back and upload the contract!!")
 
-
+    if st.session_state['contract_text'] != '':
+        prompt = st.chat_input("If you have any doubts, let us know!")
+        if prompt:
+            with st.chat_message("user"):
+                st.write(prompt)
+            with st.spinner("Answering Your Query"):
+                response = query_output(prompt)
+                with st.chat_message("bot"):
+                    st.write(response.text)
+    
 
 
 if __name__ == "__main__":
